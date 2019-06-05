087980e0cb9e09166cc2a7afad948f38411630f3
cgomezluengo <cgomezluengo@gmail.com>
2019-06-05 09:42:01 -0300

    add functionalities

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UnitService } from '../shared/unit.service';
+import { Unit } from '../shared/unit.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit  {
+  allUnit: Unit[];
+  visibleUnit: Unit[];
+  skip = 0;
+  top = 20;
+  showAnterior = false;
+  constructor(public unitService: UnitService) {}
+  
+  ngOnInit() {
+  this.mostrarUnit()
+  
+  }
 
-  constructor() {}
+  validarAnterior(){
+    this.showAnterior = (this.skip > 0)
+  }
+
+  mostrarUnit(){
+    this.validarAnterior()
+    this.unitService.getUnit(this.skip,this.top).subscribe(
+      units => {
+        this.allUnit = units;
+        this.visibleUnit = this.allUnit;
+        
+      }
+    );
+
+  }
+ 
+
+  mostrarSiguientes(){
+    
+    this.skip = this.skip + 20;
+    this.top = this.top + 20;
+    
+    this.mostrarUnit()
+
+  }
+
+  mostrarAnterior(){
+    this.skip = this.skip - 20;
+    this.top = this.top - 20;
+    this.mostrarUnit()
+
+  }
 
 }
--- src/app/shared/unit.service.ts
+++ src/app/shared/unit.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Unit } from './unit.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UnitService {
+  private unitUrl = 'http://jbjusto.dyndns.org:3001/api/testclaudio/'
+ // $top=20&$skip=0
 
-  constructor() { }
+ private unitUrl2 = 'http://jbjusto.dyndns.org:3001/api/testclaudio/$top=20&$skip=0'
+ 
+ constructor(private http: HttpClient) { }
+
+  getUnit (skip:number,top:number): Observable<Unit[]> {
+   return this.http.get<Unit[]>(this.renderURL(skip,top))
+  //return this.http.get<Unit[]>(this.unitUrl2)
+        }
+
+ renderURL (skip:number,top:number){
+  let url = this.unitUrl;
+  return url + '$top=' + top + '&$skip=' + skip;
+ }
+     
+
+
 }
